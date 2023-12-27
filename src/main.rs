954a193b6566ba966f50a9f54f6ecfed276b4c05
NSchiffmacher <nathan.schiffmacher@gmail.com>
2023-12-27 23:22:28 +0100

    Main - Add logging

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ fn main() -> Result<(), std::io::Error> {
     let height = 256;
     let mut writter: Box<dyn Writter> = Box::new(writter::PpmWritter::new("output/test.bpm".to_string(), (width, height)));
     writter.try_open()?;
+
+    print!("Starting rendering...");
+    let rendering_start = std::time::Instant::now();
     
     for y in 0..height {
         for x in 0..width {
@@ -19,8 +22,16 @@ fn main() -> Result<(), std::io::Error> {
             let b: f64 = 0.;
 
             writter.set_at((x, y), Color::new(r, g, b));
+
+            print!("\rStarting rendering... {:.2}%    ", 100. * ((height * y + x) as f64) / ((width * height) as f64));
         }
     }
 
-    writter.save()
+    print!("\rStarting rendering... Done in {:.2}s.\nSaving...", rendering_start.elapsed().as_secs_f64());
+
+    let saving_start = std::time::Instant::now();
+    writter.save()?;
+    print!("\rSaving... Saving done in {:.2}s.\n", saving_start.elapsed().as_secs_f64());
+
+    Ok(())
 }
