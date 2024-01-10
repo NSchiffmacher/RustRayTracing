028523258e75cad9bed4448a82be75933ec0c4cb
NSchiffmacher <nathan.schiffmacher@gmail.com>
2024-01-10 16:17:27 +0100

    Examples - Rewrite

---
+++ src/example/camera_blur.rs
@@ -0,0 +1,72 @@
+use raytracing::camera::Camera;
+use raytracing::color::Color;
+use raytracing::material::*;
+use raytracing::writter::{Writter, PpmWritter};
+use raytracing::vector::Point;
+use raytracing::hittable::{HittableList, Sphere};
+use raytracing::image_info::ImageInfo;
+
+use std::rc::Rc;
+
+fn main() -> Result<(), std::io::Error> {
+    camera_blur()
+}
+
+pub fn camera_blur() -> Result<(), std::io::Error> {
+    // Constants
+    const FILEPATH: &str = "output/camera_blur.ppm";
+    const WIDTH: usize = 800;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+
+    const SAMPLES_PER_PIXEL: usize = 100;
+    const MAX_DEPTH: usize = 50;
+    
+    const VERTICAL_FOV: f64 = 20.0;
+    const LOOK_FROM: Point = Point::new(-2., 2.,1.);
+    const LOOK_AT: Point = Point::new(0., 0., -1.);
+    const DEFOCUS_ANGLE: f64 = 10.0;
+    const FOCUS_DISTANCE: f64 = 3.4;
+    const UP: Point = Point::new(0., 1., 0.);
+
+    // Materials
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.5));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+
+    // Objects
+    let mut world = HittableList::new();
+    world.add(Sphere::boxed(Point::new(0., -100.5, -1.), 100., material_ground.clone()));
+    world.add(Sphere::boxed(Point::new(0., 0., -1.), 0.5, material_center.clone()));
+    world.add(Sphere::boxed(Point::new(-1., 0., -1.), 0.5, material_left.clone()));
+    world.add(Sphere::boxed(Point::new(-1., 0., -1.), -0.4, material_left.clone()));
+    world.add(Sphere::boxed(Point::new(1., 0., -1.), 0.5, material_right.clone()));
+    
+    // Image settings
+    let image_info = ImageInfo::from_aspect_ratio(
+        ASPECT_RATIO, 
+        WIDTH, 
+        FILEPATH.to_string(), 
+        SAMPLES_PER_PIXEL, 
+        MAX_DEPTH
+    );
+
+
+    // Camera 
+    let mut camera = Camera::new(VERTICAL_FOV, image_info.clone());
+    camera.set(LOOK_FROM, LOOK_AT, FOCUS_DISTANCE, DEFOCUS_ANGLE, UP);
+
+    // Output settings
+    let mut writter: Box<dyn Writter> = Box::new(PpmWritter::new(image_info.clone()));
+    writter.try_open()?;
+
+    // Rendering
+    camera.render(&world, &mut *writter);
+
+    // Saving
+    let saving_start = std::time::Instant::now();
+    writter.save()?;
+    print!("\rSaving... Saving done in {:.2}s.\n", saving_start.elapsed().as_secs_f64());
+
+    Ok(())
+}
---
+++ src/example/diffuse_lights.rs
@@ -0,0 +1,91 @@
+
+use raytracing::camera::Camera;
+use raytracing::material::*;
+use raytracing::writter::{Writter, PpmWritter};
+use raytracing::vector::{Point, Vec3};
+use raytracing::hittable::{HittableList, Sphere, Quad};
+use raytracing::image_info::ImageInfo;
+use raytracing::terminal::{Terminal, Position};
+use raytracing::texture::NoiseTexture;
+use raytracing::color::Color;
+
+use std::rc::Rc;
+
+fn main() -> Result<(), std::io::Error> {
+    diffuse_lights()
+}
+
+pub fn diffuse_lights() -> Result<(), std::io::Error> {
+    // Constants
+    const FILEPATH: &str = "output/diffuse_lights.ppm";
+    const WIDTH: usize = 400;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+
+    const SAMPLES_PER_PIXEL: usize = 1000;
+    const MAX_DEPTH: usize = 50;
+
+    const LIGHT_COLOR: Color = Color { r: 5., g: 5., b: 5. };
+    const BACKGROUND_COLOR: Color = Color { r: 0., g: 0., b: 0. };
+    
+    const VERTICAL_FOV: f64 = 20.0;
+    const LOOK_FROM: Point = Point::new(26.,3.,6.);
+    const LOOK_AT: Point = Point::new(0., 2., 0.);
+    const DEFOCUS_ANGLE: f64 = 0.;
+    const UP: Point = Point::new(0., 1., 0.);
+    let focus_distance = (LOOK_FROM - LOOK_AT).length();
+
+    welcome_message();
+
+    // Textures
+    let noise_texture = Rc::new(NoiseTexture::new(4.));
+    let diff_light = Rc::new(DiffuseLight::from_color(LIGHT_COLOR));
+
+    // Materials
+    let noise_surface = Rc::new(Lambertian::from_texture(noise_texture));
+
+    // World
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Point::new(0., -1000., 0.), 1000., noise_surface.clone())));
+    world.add(Box::new(Sphere::new(Point::new(0., 2., 0.), 2., noise_surface)));
+
+    world.add(Box::new(Quad::new(Point::new(3., 1., -2.), Vec3::new(2., 0., 0.), Vec3::new(0., 2., 0.), diff_light.clone())));
+    world.add(Box::new(Sphere::new(Point::new(0., 7., 0.), 2., diff_light)));
+    let world = world.to_bvh();
+
+    // Image settings
+    let image_info = ImageInfo::from_aspect_ratio(
+        ASPECT_RATIO, 
+        WIDTH, 
+        FILEPATH.to_string(), 
+        SAMPLES_PER_PIXEL, 
+        MAX_DEPTH
+    );
+
+    // Camera 
+    let mut camera = Camera::new(VERTICAL_FOV, image_info.clone());
+    camera.set(LOOK_FROM, LOOK_AT, focus_distance, DEFOCUS_ANGLE, UP);
+    camera.set_background(BACKGROUND_COLOR);
+
+    // Output settings
+    let mut writter: Box<dyn Writter> = Box::new(PpmWritter::new(image_info.clone()));
+    writter.try_open()?;
+
+    Terminal::cursor_position(&Position{ x: 2, y: 5});
+    println!("* Rendering image \"{}\"\r", FILEPATH);
+
+    // Rendering
+    camera.render(&world, &mut *writter);
+
+    // Saving
+    writter.save()?;
+
+    Ok(())
+}
+
+fn welcome_message() {
+    Terminal::clear_screen();
+    Terminal::cursor_position(&Position{ x: 0, y: 1});
+    println!("{}\r", Terminal::repeated('='));
+    println!("{}\r", Terminal::centered(" Shitty Raytracer ", '='));
+    println!("{}\r", Terminal::repeated('='));
+}
